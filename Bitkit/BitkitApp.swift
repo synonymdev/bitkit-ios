14cb62b37702f4884a7d2bdbba061ea37c2d4c82
Jason <jayvdb1@gmail.com>
2024-06-27 20:49:36 +0200

    feat: initial project setup

---
+++ Bitkit/BitkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitkitApp.swift
+//  Bitkit
+//
+//  Created by Jason van den Berg on 2024/06/27.
+//
+
+import SwiftUI
+
+@main
+struct BitkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bitkit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bitkit
+//
+//  Created by Jason van den Berg on 2024/06/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Bitcoin!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
