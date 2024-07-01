51173661849f70f619051dad25b1b2a7e1f0c89b
Jason <jayvdb1@gmail.com>
2024-07-01 14:18:37 +0200

    chore: move server urls to env vars

--- Bitkit/ViewModels/LightningViewModel.swift
+++ Bitkit/ViewModels/LightningViewModel.swift
@@ -13,7 +13,10 @@ class LightningViewModel: ObservableObject {
     @Published var status: NodeStatus?
     
     func start() async throws {
-        try LightningService.shared.setup()
+        let mnemonic = "science fatigue phone inner pipe solve acquire nothing birth slow armor flip debate gorilla select settle talk badge uphold firm video vibrant banner casual" // = generateEntropyMnemonic()
+        let passphrase: String? = nil
+        
+        try LightningService.shared.setup(mnemonic: mnemonic, passphrase: passphrase)
         try LightningService.shared.start()
         await sync()
     }
