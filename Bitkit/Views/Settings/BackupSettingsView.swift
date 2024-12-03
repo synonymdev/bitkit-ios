6121a684fac274d8134d40da95b5307d257a96ee
Jason <jayvdb1@gmail.com>
2024-12-03 12:34:17 +0200

    feat: backup words view

---
+++ Bitkit/Views/Settings/BackupSettingsView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct BackupSettingsView: View {
+    var body: some View {
+        List {
+            NavigationLink(destination: BackupWalletView()) {
+                Label {
+                    Text("Back up your wallet")
+                } icon: {
+                    Image(systemName: "arrow.up.doc")
+                }
+            }
+            
+            NavigationLink(destination: RestoreWalletView()) {
+                Label {
+                    Text("Reset and restore your wallet")
+                } icon: {
+                    Image(systemName: "arrow.down.doc")
+                }
+            }
+        }
+        .navigationTitle("Back Up Or Restore")
+        .navigationBarTitleDisplayMode(.large)
+    }
+}
+
+#Preview {
+    NavigationView {
+        BackupSettingsView()
+    }
+} 
