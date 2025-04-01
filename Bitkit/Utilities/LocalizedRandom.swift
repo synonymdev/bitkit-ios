a67f6b4b8e3fa7b8e824f8592b10ccf062ea93a1
Jason <jayvdb1@gmail.com>
2025-04-01 16:20:56 +0200

    feat: cjit confirmation view

--- Bitkit/Utilities/LocalizedRandom.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-func localizedRandom(_ key: String, comment: String) -> String {
-    let localizedString = NSLocalizedString(key, comment: comment)
-    let components = localizedString.components(separatedBy: "\n")
-    guard components.count > 1 else { return localizedString }
-    return components.randomElement() ?? localizedString
-}
