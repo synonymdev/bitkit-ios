10687173b516166a3b67c6819806058f77370f0d
Jason <jayvdb1@gmail.com>
2025-05-29 12:59:47 +0200

    ui: dev settings style

--- Bitkit/Views/Settings/DevSettingsView.swift
+++ Bitkit/Views/Settings/DevSettingsView.swift
@@ -8,97 +8,84 @@ struct DevSettingsView: View {
     @EnvironmentObject var widgets: WidgetsViewModel
 
     var body: some View {
-        List {
-            Section {
-                if Env.network == .regtest {
-                    NavigationLink(destination: BlocktankRegtestView()) {
-                        Label {
-                            Text("Blocktank Regtest")
-                        } icon: {
-                            Image(systemName: "hammer.fill")
-                        }
+        ScrollView {
+            if Env.network == .regtest {
+                NavigationLink(destination: BlocktankRegtestView()) {
+                    SettingsListLabel(
+                        title: "Blocktank Regtest"
+                    )
+                }
+            }
+
+            Button {
+                Task {
+                    do {
+                        try await CoreService.shared.activity.removeAll()
+                        await activity.syncState()
+                        app.toast(type: .success, title: "Success", description: "All activities removed")
+                    } catch {
+                        app.toast(type: .error, title: "Error", description: "Failed to remove activities: \(error.localizedDescription)")
                     }
                 }
+            } label: {
+                SettingsListLabel(
+                    title: "Reset All Activities",
+                    rightIcon: nil
+                )
+            }
 
-                Button {
-                    Task {
-                        do {
-                            try await CoreService.shared.activity.removeAll()
-                            await activity.syncState()
-                            app.toast(type: .success, title: "Success", description: "All activities removed")
-                        } catch {
-                            app.toast(type: .error, title: "Error", description: "Failed to remove activities: \(error.localizedDescription)")
-                        }
+            Button {
+                Task {
+                    do {
+                        try await CoreService.shared.activity.generateRandomTestData(count: 100)
+                        await activity.syncState()
+                        app.toast(type: .success, title: "Success", description: "Generated 100 random activities")
+                    } catch {
+                        app.toast(type: .error, title: "Error", description: "Failed to generate activities: \(error.localizedDescription)")
                     }
-                } label: {
-                    Label {
-                        Text("Reset All Activities")
-                    } icon: {
-                        Image(systemName: "clock.badge.xmark")
-                    }
-                    .foregroundColor(.redAccent)
                 }
+            } label: {
+                SettingsListLabel(
+                    title: "Generate Test Activities",
+                    rightIcon: nil
+                )
+            }
 
-                Button {
-                    Task {
-                        do {
-                            try await CoreService.shared.activity.generateRandomTestData(count: 100)
-                            await activity.syncState()
-                            app.toast(type: .success, title: "Success", description: "Generated 100 random activities")
-                        } catch {
-                            app.toast(type: .error, title: "Error", description: "Failed to generate activities: \(error.localizedDescription)")
+            Button {
+                Task {
+                    guard Env.network == .regtest else {
+                        Logger.error("Can only nuke on regtest")
+                        app.toast(type: .error, title: "Error", description: "Can only nuke on regtest")
+                        return
+                    }
+                    do {
+                        // TODO: reset all of app state
+                        navigation.reset()
+                        app.hasSeenTransferToSavingsIntro = false
+                        app.hasSeenTransferToSpendingIntro = false
+                        app.hasSeenWidgetsIntro = false
+                        widgets.clearWidgets()
+
+                        if wallet.nodeLifecycleState == .running || wallet.nodeLifecycleState == .starting
+                            || wallet.nodeLifecycleState == .stopping
+                        {
+                            try await wallet.wipeLightningWallet()
                         }
+                        try await CoreService.shared.activity.removeAll()
+                        try Keychain.wipeEntireKeychain()
+                        try wallet.setWalletExistsState()
+                    } catch {
+                        app.toast(error)
                     }
-                } label: {
-                    Label {
-                        Text("Generate Test Activities")
-                    } icon: {
-                        Image(systemName: "ladybug.fill")
-                    }
-                    .foregroundColor(.orange)
                 }
-
-                Button {
-                    Task {
-                        guard Env.network == .regtest else {
-                            Logger.error("Can only nuke on regtest")
-                            app.toast(type: .error, title: "Error", description: "Can only nuke on regtest")
-                            return
-                        }
-                        do {
-                            // TODO: reset all of app state
-                            navigation.reset()
-                            app.hasSeenTransferToSavingsIntro = false
-                            app.hasSeenTransferToSpendingIntro = false
-                            app.hasSeenWidgetsIntro = false
-                            widgets.clearWidgets()
-
-                            if wallet.nodeLifecycleState == .running || wallet.nodeLifecycleState == .starting
-                                || wallet.nodeLifecycleState == .stopping
-                            {
-                                try await wallet.wipeLightningWallet()
-                            }
-                            try await CoreService.shared.activity.removeAll()
-                            try Keychain.wipeEntireKeychain()
-                            try wallet.setWalletExistsState()
-                        } catch {
-                            app.toast(error)
-                        }
-                    }
-                } label: {
-                    Label {
-                        Text("Wipe Wallet")
-                    } icon: {
-                        Image(systemName: "trash.fill")
-                    }
-                    .foregroundColor(.redAccent)
-                }
-            } header: {
-                Text("Development Tools")
+            } label: {
+                SettingsListLabel(
+                    title: "Wipe Wallet",
+                    rightIcon: nil
+                )
             }
         }
         .navigationTitle("Dev Settings")
-        .navigationBarTitleDisplayMode(.large)
     }
 }
 
