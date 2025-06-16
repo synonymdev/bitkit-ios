78f172f8c82b6c1b8697273d1b3ba8b71665a7f9
Jason <jayvdb1@gmail.com>
2025-06-16 14:45:47 +0200

    fix: serialize tests

--- manually-test-payments.sh
+++ manually-test-payments.sh
@@ -4,4 +4,5 @@ xcodebuild test \
   -scheme Bitkit \
   -destination "name=iPhone 16" \
   -only-testing:BitkitTests/PaymentFlowTests/testPaymentFlow \
+  -parallel-testing-enabled NO \
   GCC_WARN_INHIBIT_ALL_WARNINGS=YES | xcpretty | grep -v "⚠<fe0f>"
