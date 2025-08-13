a3bdaff8a6c17c8624dac051979fec1a507644bd
Philipp Walter <philippwalter@pm.me>
2025-08-13 11:33:55 +0200

    fix(receive): add image to share sheet (#128)

---
+++ Bitkit/Components/ShareSheet.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+import UIKit
+
+struct ShareSheet: UIViewControllerRepresentable {
+    let activityItems: [Any]
+
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        let controller = UIActivityViewController(
+            activityItems: activityItems,
+            applicationActivities: nil
+        )
+        return controller
+    }
+
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
+}
