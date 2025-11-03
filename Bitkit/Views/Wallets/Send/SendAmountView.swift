6752c0b13ebf98565ec3acbf596a3e8662777e16
Piotr Stachyra <piotr.stachyra@gmail.com>
2025-11-03 17:11:03 +0100

    AvailableAmount

--- Bitkit/Views/Wallets/Send/SendAmountView.swift
+++ Bitkit/Views/Wallets/Send/SendAmountView.swift
@@ -61,7 +61,8 @@ struct SendAmountView: View {
                 HStack(alignment: .bottom) {
                     AvailableAmount(
                         label: t("wallet__send_available"),
-                        amount: Int(availableAmount)
+                        amount: Int(availableAmount),
+                        testIdentifier: "AvailableAmount"
                     )
                     .onTapGesture {
                         amountViewModel.updateFromSats(availableAmount, currency: currency)
