15442a64c302323a141af6f858047e8975d1f009
Piotr Stachyra <piotr.stachyra@gmail.com>
2025-11-13 16:14:19 +0100

    General settings test ids

--- Bitkit/Views/Settings/GeneralSettingsView.swift
+++ Bitkit/Views/Settings/GeneralSettingsView.swift
@@ -26,14 +26,15 @@ struct GeneralSettingsView: View {
                             rightText: currency.selectedCurrency
                         )
                     }
+                    .accessibilityIdentifier("CurrenciesSettings")
 
                     NavigationLink(value: Route.unitSettings) {
                         SettingsListLabel(
                             title: t("settings__general__unit"),
                             rightText: currency.primaryDisplay == .bitcoin ? currency.primaryDisplay.rawValue : currency.selectedCurrency
                         )
-                        .accessibilityIdentifier("UnitSettings")
                     }
+                    .accessibilityIdentifier("UnitSettings")
 
                     NavigationLink(value: Route.transactionSpeedSettings) {
                         SettingsListLabel(
@@ -41,6 +42,7 @@ struct GeneralSettingsView: View {
                             rightText: settings.defaultTransactionSpeed.displayTitle
                         )
                     }
+                    .accessibilityIdentifier("TransactionSpeedSettings")
 
                     if !tagManager.lastUsedTags.isEmpty {
                         NavigationLink(value: Route.tagSettings) {
@@ -49,6 +51,7 @@ struct GeneralSettingsView: View {
                                 rightText: String(tagManager.lastUsedTags.count)
                             )
                         }
+                        .accessibilityIdentifier("TagsSettings")
                     }
 
                     NavigationLink(value: Route.widgetsSettings) {
@@ -57,6 +60,7 @@ struct GeneralSettingsView: View {
                             rightText: settings.showWidgets ? tTodo("On") : tTodo("Off")
                         )
                     }
+                    .accessibilityIdentifier("WidgetsSettings")
 
                     NavigationLink(value: app.hasSeenQuickpayIntro ? Route.quickpay : Route.quickpayIntro) {
                         SettingsListLabel(
@@ -64,6 +68,7 @@ struct GeneralSettingsView: View {
                             rightText: settings.enableQuickpay ? tTodo("On") : tTodo("Off")
                         )
                     }
+                    .accessibilityIdentifier("QuickpaySettings")
 
                     NavigationLink(value: app.hasSeenNotificationsIntro ? Route.notifications : Route.notificationsIntro) {
                         SettingsListLabel(
@@ -71,6 +76,7 @@ struct GeneralSettingsView: View {
                             rightText: settings.enableNotifications ? tTodo("On") : tTodo("Off")
                         )
                     }
+                    .accessibilityIdentifier("NotificationsSettings")
                 }
             }
         }
