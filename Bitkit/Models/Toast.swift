0d02dfdf9feca7c8b20c3c02c6bb09d3e7652471
Philipp Walter <philippwalter@proton.me>
2025-11-28 19:58:56 +0100

    fix(ui): polish toast

--- Bitkit/Models/Toast.swift
+++ Bitkit/Models/Toast.swift
@@ -5,9 +5,19 @@ struct Toast: Equatable {
         case success, info, lightning, warning, error
     }
 
+    let id: UUID
     let type: ToastType
     let title: String
     let description: String?
     let autoHide: Bool
     let visibilityTime: Double
+
+    init(id: UUID = UUID(), type: ToastType, title: String, description: String? = nil, autoHide: Bool = true, visibilityTime: Double = 4.0) {
+        self.id = id
+        self.type = type
+        self.title = title
+        self.description = description
+        self.autoHide = autoHide
+        self.visibilityTime = visibilityTime
+    }
 }
