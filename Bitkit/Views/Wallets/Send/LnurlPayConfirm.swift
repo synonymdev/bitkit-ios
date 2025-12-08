a215c6b9cc87397343ed3aeac59e32204871d5d8
Piotr Stachyra <piotr.stachyra@gmail.com>
2025-12-08 15:36:34 +0100

    able to dissmisskeyboard on lnurl comment

--- Bitkit/Views/Wallets/Send/LnurlPayConfirm.swift
+++ Bitkit/Views/Wallets/Send/LnurlPayConfirm.swift
@@ -16,6 +16,7 @@ struct LnurlPayConfirm: View {
     @State private var showingBiometricError = false
     @State private var biometricErrorMessage = ""
     @State private var comment = ""
+    @FocusState private var isCommentFocused: Bool
 
     private var biometryTypeName: String {
         switch Env.biometryType {
@@ -43,8 +44,12 @@ struct LnurlPayConfirm: View {
             SheetHeader(title: t("wallet__lnurl_p_title"), showBackButton: true)
 
             VStack(alignment: .leading) {
-                MoneyStack(sats: Int(wallet.sendAmountSats ?? app.lnurlPayData!.minSendable), showSymbol: true)
-                    .padding(.bottom, 32)
+                MoneyStack(
+                    sats: Int(wallet.sendAmountSats ?? app.lnurlPayData!.minSendable),
+                    showSymbol: true,
+                    testIdPrefix: "ReviewAmount"
+                )
+                .padding(.bottom, 32)
 
                 VStack(spacing: 0) {
                     VStack(alignment: .leading) {
@@ -86,8 +91,11 @@ struct LnurlPayConfirm: View {
                                 t("wallet__lnurl_pay_confirm__comment_placeholder"),
                                 text: $comment,
                                 axis: .vertical,
-                                testIdentifier: "CommentInput"
+                                testIdentifier: "CommentInput",
+                                submitLabel: .done
                             )
+                            .focused($isCommentFocused)
+                            .dismissKeyboardOnReturn(text: $comment, isFocused: $isCommentFocused)
                             .lineLimit(3 ... 3)
                             .onChange(of: comment) { newValue in
                                 let maxLength = Int(commentAllowed)
