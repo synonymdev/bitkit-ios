098bdec5b30889a0543fc00c8ee48f0b629da347
jvsena42 <jvsena16@gmail.com>
2026-01-02 08:10:10 -0300

    chore: add keychain key to wipe method

--- Bitkit/Utilities/AppReset.swift
+++ Bitkit/Utilities/AppReset.swift
@@ -29,9 +29,18 @@ enum AppReset {
         // Wipe keychain
         try Keychain.wipeEntireKeychain()
 
+        // Wipe encryption key
+        try KeychainCrypto.deleteKey()
+
         // Wipe user defaults
         if let bundleID = Bundle.main.bundleIdentifier {
             UserDefaults.standard.removePersistentDomain(forName: bundleID)
+        }
+
+        // Wipe App Group UserDefaults
+        if let appGroupDefaults = UserDefaults(suiteName: "group.bitkit") {
+            appGroupDefaults.removePersistentDomain(forName: "group.bitkit")
+            Logger.info("Wiped App Group UserDefaults", context: "AppReset")
         }
 
         // Wipe logs
