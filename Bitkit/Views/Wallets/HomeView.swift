b243e27ba06d7cdac5bb8373fdef3efba0ee0a5f
Philipp Walter <philippwalter@pm.me>
2026-01-23 19:36:19 +0100

    fix(widgets): #392 allow scrolling when editing (#395)

--- Bitkit/Views/Wallets/HomeView.swift
+++ Bitkit/Views/Wallets/HomeView.swift
@@ -66,7 +66,6 @@ struct HomeView: View {
                     .padding(.bottom, 130)
                 }
             }
-            .scrollDisabled(isEditingWidgets)
 
             // Gradients layer
             VStack(spacing: 0) {
