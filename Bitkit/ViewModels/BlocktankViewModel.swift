02ff4c85523080f7a5b1d928e0fc2bf92a993209
Philipp Walter <philippwalter@proton.me>
2026-03-06 12:01:51 +0100

    fix(transfer): update order default "source"

--- Bitkit/ViewModels/BlocktankViewModel.swift
+++ Bitkit/ViewModels/BlocktankViewModel.swift
@@ -7,7 +7,7 @@ class BlocktankViewModel: ObservableObject {
     @Published var cJitEntries: [IcJitEntry]? = nil
     @Published var info: IBtInfo? = nil
 
-    // Use -1 as a sentinel value to represent nil
+    /// Use -1 as a sentinel value to represent nil
     @AppStorage("minCjitSats") private var minCjitSatsStorage: Int = -1
 
     var minCjitSats: UInt64? {
@@ -16,7 +16,7 @@ class BlocktankViewModel: ObservableObject {
     }
 
     private let defaultChannelExpiryWeeks: UInt32 = 6
-    private let defaultSource = "bitkit"
+    private let defaultSource = "bitkit-ios"
 
     @Published private(set) var isRefreshing = false
 
