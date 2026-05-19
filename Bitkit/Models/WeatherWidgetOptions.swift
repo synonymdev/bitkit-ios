f96e4d25a509cfe3595d8ad3018a32d73d93fec7
jvsena42 <jvsena16@gmail.com>
2026-05-19 07:12:10 -0300

    refactor: comments cleanup

--- Bitkit/Models/WeatherWidgetOptions.swift
+++ Bitkit/Models/WeatherWidgetOptions.swift
@@ -1,13 +1,11 @@
 import Foundation
 
-/// Single-select display metric used by the v61 Weather widget.
 enum WeatherDisplayMetric: String, Codable, CaseIterable {
     case fiatFee
     case satsFee
     case nextBlockFee
 }
 
-/// Options for configuring the in-app and home-screen Weather widgets (shared via App Group).
 struct WeatherWidgetOptions: Codable, Equatable {
     var selectedMetric: WeatherDisplayMetric = .fiatFee
 
