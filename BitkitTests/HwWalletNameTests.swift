146bee431a6502f0541758ff90bde7e8ca28045c
jvsena42 <jvsena16@gmail.com>
2026-06-29 13:59:33 -0300

    test: add name tests

--- BitkitTests/HwWalletNameTests.swift
+++ BitkitTests/HwWalletNameTests.swift
@@ -25,4 +25,25 @@ final class HwWalletNameTests: XCTestCase {
     func testEmptyLabelFallsBackToModel() {
         XCTAssertEqual(resolveHwWalletName(label: "", model: "Safe 5"), "Trezor Safe 5")
     }
+
+    func testCustomLabelTakesPriorityOverLabelAndModel() {
+        XCTAssertEqual(
+            resolveHwWalletName(label: "My Trezor", model: "Safe 5", customLabel: "Cold Storage"),
+            "Cold Storage"
+        )
+    }
+
+    func testEmptyCustomLabelFallsBackToLabel() {
+        XCTAssertEqual(
+            resolveHwWalletName(label: "My Trezor", model: "Safe 5", customLabel: ""),
+            "My Trezor"
+        )
+    }
+
+    func testNilCustomLabelFallsBackToPrefixedModel() {
+        XCTAssertEqual(
+            resolveHwWalletName(label: nil, model: "Safe 5", customLabel: nil),
+            "Trezor Safe 5"
+        )
+    }
 }
